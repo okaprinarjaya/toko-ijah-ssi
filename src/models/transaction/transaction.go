995ef99fe5a3c7afafb415b0f7e36ca8c261cdba
Oka Prinarjaya <okaprinarjaya@gmail.com>
2018-01-15 11:04:41 +0700

    Update transaction detail information, Deliver

--- src/models/transaction/transaction.go
+++ src/models/transaction/transaction.go
@@ -7,20 +7,28 @@ import (
 )
 
 type Transaction struct {
-    TrxId string `json:"trx_id"`
-    TrxType string `json:"trx_type"`
-    Notes string `json:"notes"`
-    Created string `json:"created"`
-    Updated string `json:"updated"`
+    TrxId     string               `json:"trx_id"`
+    TrxType   string               `json:"trx_type"`
+    Notes     string               `json:"notes"`
+    Created   string               `json:"created"`
+    Updated   string               `json:"updated"`
     TrxDetail [] TransactionDetail `json:"trx_detail"`
 }
 
 type TransactionDetail struct {
-    Id int `json:"id"`
-    TrxId string `json:"trx_id"`
-    Sku string `json:"sku"`
-    Quantity int `json:"quantity"`
-    Notes string `json:"notes"`
+    Id              int    `json:"id"`
+    TrxId           string `json:"trx_id"`
+    Sku             string `json:"sku"`
+    PriceBuy        int    `json:"price_buy"`
+    PriceSale       int    `json:"price_sale"`
+    OrderQuantity   int    `json:"order_quantity"`
+    ReceiveQuantity int    `json:"receive_quantity"`
+    QuantityOut     int    `json:"quantity_out"`
+    KwitansiNumber  string `json:"kwitansi_number"`
+    Notes           string `json:"notes"`
+    ItemName        string `json:"item_name"`
+    TotalPriceBuy   int `json:"total_price_buy"`
+    TotalPriceSale  int `json:"total_price_sale"`
 }
 
 func (trx *Transaction) GenerateTable(db *sql.DB) {
@@ -37,7 +45,12 @@ func (trx *Transaction) GenerateTable(db *sql.DB) {
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         trx_id VARCHAR(32) NOT NULL,
         sku VARCHAR(32) NOT NULL,
-        quantity INTEGER NOT NULL,
+        price_buy INTEGER,
+        price_sale INTEGER,
+        order_quantity INTEGER,
+        receive_quantity INTEGER,
+        quantity_out INTEGER,
+        kwitansi_number VARCHAR(32) NOT NULL,
         notes TEXT NULL
     );
     `
@@ -68,12 +81,15 @@ func (trx *Transaction) Create(db *sql.DB) (sql.Result, error) {
 }
 
 func createDetails(db *sql.DB, trxCode string, trxDetail [] TransactionDetail) (sql.Result, error) {
-    sqlStr := "INSERT INTO transactions_details (trx_id, sku, quantity, notes) VALUES "
+    sqlStr := "INSERT INTO transactions_details (trx_id, sku, price_buy, price_sale, order_quantity, " +
+        "receive_quantity, quantity_out, kwitansi_number, notes) VALUES "
     var values [] interface{}
 
     for _, trxItem := range trxDetail {
-        sqlStr += "(?,?,?,?),"
-        values = append(values, trxCode, trxItem.Sku, trxItem.Quantity, trxItem.Notes)
+        sqlStr += "(?,?,?,?,?,?,?,?,?),"
+        values = append(values,
+            trxCode, trxItem.Sku, trxItem.PriceBuy, trxItem.PriceSale, trxItem.OrderQuantity,
+            trxItem.ReceiveQuantity, trxItem.QuantityOut, trxItem.KwitansiNumber, trxItem.Notes)
     }
 
     sqlStr = strings.TrimSuffix(sqlStr, ",")
@@ -92,7 +108,7 @@ func createDetails(db *sql.DB, trxCode string, trxDetail [] TransactionDetail) (
     return result, nil
 }
 
-func (trx *Transaction) GetTransactions(db *sql.DB, trxType string) ([] Transaction, error)  {
+func (trx *Transaction) GetTransactions(db *sql.DB, trxType string) ([] Transaction, error) {
     rows, err := db.Query(
         "SELECT trx_id, trx_type, notes, created, updated FROM transactions WHERE trx_type = $1",
         trxType)
@@ -117,11 +133,26 @@ func (trx *Transaction) GetTransactions(db *sql.DB, trxType string) ([] Transact
     return transactions, nil
 }
 
-func (trxDetails *TransactionDetail) GetTransactionDetails(db *sql.DB, trxCode string) ([] TransactionDetail, error)  {
-    rows, err := db.Query("SELECT id, trx_id, sku, quantity, notes FROM transactions_details " +
-        "WHERE trx_id = $1",
-        trxCode)
+func (trxDetails *TransactionDetail) GetTransactionDetails(db *sql.DB, trxCode string) ([] TransactionDetail, error) {
+    sqlStr :=
+    "SELECT " +
+       "trxd.id, " +
+       "trxd.trx_id, " +
+       "trxd.sku, " +
+       "itm.item_name, " +
+       "order_quantity, " +
+       "receive_quantity, " +
+       "trxd.price_buy, " +
+       "(trxd.price_buy * trxd.order_quantity) AS total_price_buy, " +
+       "(trxd.price_sale * trxd.quantity_out) AS total_price_sale, " +
+       "trxd.quantity_out, " +
+       "trxd.price_sale, " +
+       "trxd.kwitansi_number, " +
+       "trxd.notes " +
+    "FROM transactions_details trxd JOIN items itm ON trxd.sku = itm.sku " +
+    "WHERE trxd.trx_id = $1"
 
+    rows, err := db.Query(sqlStr, trxCode)
     if err != nil {
         return nil, err
     }
@@ -131,7 +162,21 @@ func (trxDetails *TransactionDetail) GetTransactionDetails(db *sql.DB, trxCode s
     var trxDetailItems [] TransactionDetail
     for rows.Next() {
         var trxItem TransactionDetail
-        err := rows.Scan(&trxItem.Id, &trxItem.TrxId, &trxItem.Sku, &trxItem.Quantity, &trxItem.Notes)
+        err := rows.Scan(
+            &trxItem.Id,
+            &trxItem.TrxId,
+            &trxItem.Sku,
+            &trxItem.ItemName,
+            &trxItem.OrderQuantity,
+            &trxItem.ReceiveQuantity,
+            &trxItem.PriceBuy,
+            &trxItem.TotalPriceBuy,
+            &trxItem.TotalPriceSale,
+            &trxItem.QuantityOut,
+            &trxItem.PriceSale,
+            &trxItem.KwitansiNumber,
+            &trxItem.Notes)
+
         if err != nil {
             return nil, err
         }
